10f4ac7a77576d5e632c8e32c2492144bd67573c
mindycook <melindacook2002@gmail.com>
2025-01-15 14:15:34 -0500

    myCommit

--- mcookexercise.cpp
+++ mcookexercise.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello world.\n");
+    int temp1 = 70;
+    int temp2 = 85;
+    int myTemp;
+
+    printf("Please input a temperature\n");
+    scanf("%d", &myTemp);
+    printf("Result: ");
+    bool evaluate = myTemp > temp1 && myTemp < temp2;
+    printf("%d\n",evaluate);
+    return 0;
+
 
 }
