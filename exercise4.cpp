fb8a27d273c78c2092f9490100fc9e97b7b100d4
mindycook <melindacook2002@gmail.com>
2025-01-22 14:06:51 -0500

    fixed array

--- exercise4.cpp
+++ exercise4.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 int main() {
-// Another way to access an Array using the pointer address
-for (int i = 0; i < 4; i++){
-	printf("%d\n", *(myNumbers+i));
-}
-return 0;
+    int myNumbers[4] = {25, 750, 75, 100};
+    // Another way to access an Array using the pointer address
+    int length = sizeof(myNumbers)/sizeof(myNumbers[0]);
+    for (int i = 0; i < length; i++){
+        printf("%d\n", *(myNumbers+i));
+    }
+    return 0;
 }
